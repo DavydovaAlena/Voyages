ee3ccc3dfabbe58869b21fd7f31d19eea1ee9c16
Davydova Alena <davydovaalena605@gmail.com>
2024-07-16 12:30:04 +0300

    hello

--- app/src/main/java/ru/adavydova/voyages/VoyagesApp.kt
+++ app/src/main/java/ru/adavydova/voyages/VoyagesApp.kt
@@ -11,6 +11,7 @@ import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class VoyagesApp: Application(), ImageLoaderFactory {
+    //hello
     override fun newImageLoader(): ImageLoader {
         return ImageLoader(this).newBuilder()
             .memoryCachePolicy(CachePolicy.ENABLED)
